39dd820905b77fd67fa06e78746671a822354c5b
Carter David Henning <carterdavidhenning@gmail.com>
2021-06-05 18:34:30 -0400

    Feat: Add simple player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Collider2D col;
+
+    [SerializeField]
+    private float moveSpeed = 3;
+
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        Debug.Assert(rb != null);
+        Debug.Assert(col != null);
+
+        moveInput = Vector2.zero;
+    }
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(moveInput * moveSpeed, ForceMode2D.Force);
+    }
+}
