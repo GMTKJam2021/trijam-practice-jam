aec3ed66d7b60b946a342a0f2b6095b26fc604ed
Ahnaf Hassan <absuloutezero@gmail.com>
2021-06-06 13:25:25 -0400

    Buttons, Keys, and Player Interaction
    Added player interactivity with keys and buttons, both of which open doors. Keys open doors permanently, buttons open them for 3 seconds.
    
    Also added a sandbox scene to test out these new interactions.

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Button"))
+        {
+            collision.GetComponent<ButtonController>().Pressed();
+        }
+
+        if (collision.CompareTag("Key"))
+        {
+            collision.GetComponent<KeyController>().Unlock();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Button"))
+        {
+            collision.GetComponent<ButtonController>().Unpressed();
+        }
+    }
+}
